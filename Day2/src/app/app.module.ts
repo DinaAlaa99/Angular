6bdbcba9bdb686d7b3815b2dd90cf6a35f324a09
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-05-09 15:35:20 +0200

    add core module

--- Day2/src/app/app.module.ts
+++ Day2/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DepartmentComponent } from './department/department.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    StudentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
