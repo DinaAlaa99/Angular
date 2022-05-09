4cedd7242f27222ef1935721a44cf93c13bc92e4
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-05-09 14:45:10 +0200

    add department component

--- Day2/src/app/app.module.ts
+++ Day2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule
